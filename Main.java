078ec27ff237dac7b9b32c4a3ec55f0aae02297e
Ondakanov <abzalkhan.ondakanov.000322550189@gmail.com>
2022-05-23 13:24:03 +0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        float first, second, result;
+        System.out.println("Enter first num :");
+        first = num.nextFloat();
+        System.out.println("Enter second num :");
+        second = num.nextFloat();
+        result = first + second;
+        System.out.println("Result is - " + result);
+    }
+
+}
